a71eeb7bcbc263710ee432ed75900ddfad131de4
William Washington <WWashington@edfinancial.com>
2018-12-04 15:15:09 -0500

    Add project files.

---
+++ PowerShellProject2/PowerShellProject2.ps1
@@ -0,0 +1,17 @@
+﻿#
+# Script.ps1
+#
+
+Clear-Content "c:\testEnv\kaceRegexPCNames\results.txt"
+
+#Add-Content "C:\testEnv\test.txt" "|"
+
+$csv = Get-Content "c:\testEnv\kaceRegexPCNames\pcNames.csv"
+Add-Content "c:\testEnv\kaceRegexPCNames\results.txt" "^("
+
+foreach ($pc in $csv) {
+    Add-Content "c:\testEnv\kaceRegexPCNames\results.txt" $pc"|"
+}
+Add-Content "c:\testEnv\kaceRegexPCNames\results.txt" ")$"
+
+(gc "c:\testEnv\kaceRegexPCNames\kaceRegexPCNames\results.txt") | ? {$_.trim() -ne "" } | set-content "c:\testEnv\kaceRegexPCNames\kaceRegexPCNames\results.txt"
